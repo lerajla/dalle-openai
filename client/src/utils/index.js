62dad05c9a3c13bb4b27f74025f3610a1a86ef45
AjlaLer <ajller@harpaitalia.it>
2024-06-24 11:49:39 +0200

    dalle openai project

---
+++ client/src/utils/index.js
@@ -0,0 +1,12 @@
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt() {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) {
+    return getRandomPrompt();
+  }
+
+  return randomPrompt;
+}
